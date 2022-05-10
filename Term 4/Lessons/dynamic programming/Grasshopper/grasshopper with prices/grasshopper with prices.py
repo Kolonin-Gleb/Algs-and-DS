82fe1f0e59ca5fedebc851974f440e55839e6e35
Kolonin-Gleb <56352942+Kolonin-Gleb@users.noreply.github.com>
2022-05-10 06:22:22 +0300

    Решение колледжевых задач

---
+++ Term 4/Lessons/dynamic programming/Grasshopper/grasshopper with prices/grasshopper with prices.py
@@ -0,0 +1,7 @@
+# Одномерное динамическое программирование
+# Урок, где разбирается задача:
+# https://foxford.ru/wiki/informatika/odnomernoe-dinamicheskoe-programmirovanie-nailuchshiy-sposob
+
+
+
+
