eedcf2bffca1110c6ddcf3e1a4fc03a614d856ff
Kolonin-Gleb <56352942+Kolonin-Gleb@users.noreply.github.com>
2022-05-21 07:26:16 +0300

    Какими купюрами выдавать

--- Term 4/Lessons/dynamic programming/2_ATM/ATM.py
+++ Term 4/Lessons/dynamic programming/2_ATM/ATM.py
@@ -3,19 +3,20 @@
 
 import math
 
-
-banknotes = [10, 60, 100] # Доступные банкноты
-sum = 120 # Сумма для размена
+banknotes = [30, 50, 100] # Доступные банкноты
+sum = 210 # Сумма для размена
 
 # База (0 можно разменять 0 купюр, не получится разменять)
 # Будет хранить минимальное кол. купюр, которыми можно получить сумму =
 # значению индекса, в котором хранится число купюр
 # ВАЖНО: Если тестируешь на маленькой сумме, то измени БАЗУ
 best_exchanges = [math.inf] * (sum + 1)
+
+# Установка базы
 best_exchanges[0] = 0
-best_exchanges[10] = 1
-best_exchanges[60] = 1
-best_exchanges[100] = 1
+for i in banknotes:
+	best_exchanges[i] = 1
+
 
 def ATM_min_exchange(banknotes, best_exchanges, sum):
 	# Если лучший исход для запрашиваемой суммы ещё не определён
@@ -36,9 +37,30 @@ def ATM_min_exchange(banknotes, best_exchanges, sum):
 	
 	return best_exchanges[sum]
 
-print("Минимальное число купюр, необходимое для выдачи суммы = " + str(sum) + " = ")
-print(ATM_min_exchange(banknotes, best_exchanges, sum))
+def ATM_min_exchange_path(sum):
+	ans = ATM_min_exchange(banknotes, best_exchanges, sum) # Сколько купюр нужно дать
+	dig = sum # Какую сумму выдать
+	path = [] # Какие купюры давать
 
-print(best_exchanges)
+	while best_exchanges[dig] != 1: # Пока от суммы не останется часть, которую можно выдать 1 купюрой
+		for i in banknotes:
+			if (dig - i) > 0 and best_exchanges[dig - i] == ans-1:
+				# Если из суммы можно выдать часть = купюре И 
+				# для оставшейся части суммы останется отдать на 1 купюру меньше
+				
+				path.append(i) # Эта купюра используется в выдаче
+				dig = dig-i # Остаётся отдать меньше на сумму = купюре
+				ans = ans-1 # Для выдачи остаётся на 1 купюру меньше
+			
+	path.append(dig) # Добавляем последнюю купюру
+	return path
 
-# TODO: Как усовершенствовать программу, чтобы определить какие купюры нужно давать?
+
+min_banknotes = ATM_min_exchange(banknotes, best_exchanges, sum)
+print("Минимальное число купюр, необходимое для выдачи " + str(sum) + " = " + str(min_banknotes))
+
+#print("Лучшие размены для всех сумм до запрашиваемой:")
+#print(best_exchanges)
+
+print("Купюры, которые нужно выдать:")
+print(ATM_min_exchange_path(sum))
