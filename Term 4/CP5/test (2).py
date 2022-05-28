43e7149e094f0c881e169d8a0abd09eaaaa4bcbe
Kolonin-Gleb <56352942+Kolonin-Gleb@users.noreply.github.com>
2022-05-28 18:05:16 +0300

    Продолжаю решать КТ

--- Term 4/CP5/test (2).py
+++ Term 4/CP5/test (2).py
@@ -3,54 +3,27 @@
 import math
 
 
-position = 0
+cur_position = 0
 target_position = 5
 k = 3
 prices = [0, -7, -5, -6, -4, 4, 2, 0]
 
-price_to_come = [-math.inf] * (target_position - position)
+price_to_come = [-math.inf] * (target_position - cur_position)
 
-for i in range(len(price_to_come)):
-    # Если позади элементов меньше, чем возможность прыжка
-    # То суммируем значения тех эл., что имеются и +1, т.к. можно допрыгнуть из тек. положения
+next_position = cur_position + 1
 
+# Определение Лучших способов добраться в те позиции, до которых можно сразу допрыгнуть
+for i in range(next_position, cur_position + k + 1): #TODO: Как быть, если k = 1?
+	# Прямой способ добраться до позиции всегда самый выгодный, т.к. все предыдущие элементы < 0
+	price_to_come[i - 1] = prices[i]
 
-	# Определение Лучших способов добраться в те позиции, до которых можно сразу допрыгнуть
-	next_position = position + 1 # Высчитываю стоимость добраться до следующей позиции
+# Определение лучших способов добраться в удаленные позиции
+for y in range(cur_position + k + 1, target_position + 1):
+	# Сохраняю все стоимости добаться до позиции
+	possible_prices = []
+	for j in range(y-k-1, y - 1): # Рассматриваю k предыдущих цен
+		possible_prices.append(price_to_come[j] + prices[y]) # TODO: Проблема с индексами prices
+	price_to_come[y - 1] = max(possible_prices)
 
-	for i in range(position + 1, position + k):
-		# Прямой способ добраться до позиции всегда самый выгодный, т.к. все предыдущие элементы < 0
-		price_to_come[i] = prices[i]
-		break
-		#else: # Сохраняю все способы добаться до следующей позиции
-		#	possible_prices = []
-		#	possible_prices.append(prices[i]) # Прямой способ
-
-		#	# Может ли быть такое, что прямой способ всегда самый выгодный?
-		#	# Да, прямой ход всегда самый выгодный, т.к. если есть + число до которого можно сразу допрыгнуть, я делаю это автоматически!
-
-		#	#for j in range(1, k)
-		#	#possible_prices.append(price_to_come[i - 1])
-
-		print(ways_to_come)
-    else:
-		break
+print(price_to_come)
 		
-
-
-# Число способов прийти в те элементы, до которых нельзя допрыгнуть
-ways_to_come.append(0)
-ways_to_come.append(0)
-ways_to_come.append(0)
-
-k = 3 # Сила прыжка
-position = 3 # Элемент, до которого нужно определить лучший способ добраться
-
-for i in range(position, len(ways_to_come)):
-	# Определяем предшествующие текущему элементы
-	previous_elements = []
-	for j in range(k):
-		previous_elements.append()
-
-	ways_to_come[i] = max()
-
---
+++ Term 4/CP5/testik.py
@@ -0,0 +1,5 @@
+k = 3
+
+for j in range(k, 0, -1):
+	print(j)
+
