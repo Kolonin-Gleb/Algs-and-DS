8ad7bf57f5eae038cb8c4aec0779c7013d6f15de
Kolonin-Gleb <56352942+Kolonin-Gleb@users.noreply.github.com>
2022-05-28 14:13:59 +0300

    Решение КТ

---
+++ Term 4/CP5/test.py
@@ -0,0 +1,38 @@
+# ЭТОТ КОД НУЖНО ПЕРЕПИСАТЬ!
+# Я НЕ ЧИСЛО СПОСОБОВ ДОЛЖЕН ОПРЕДЕЛИТЬ, А СТОИМОСТИ КАЖДОГО СПОСОБА!
+
+
+# ways_to_come - кол. способов прийти в те элементы, до которых можно допрыгнуть
+ways_to_come = [1, 1, 1]
+
+count_of_prev_elements = 0
+for i in range(len(ways_to_come)):
+	count_of_prev_elements = i - 1
+
+	while True: # Суммирую кол. способов добраться до пред эл.
+		if i == 0:
+			ways_to_come[i] = 1
+			break
+		else:
+			ways_to_come[i] += ways_to_come[count_of_prev_elements]
+			count_of_prev_elements -= 1
+			if count_of_prev_elements == -1:
+				break
+print(ways_to_come)
+
+# Число способов прийти в те элементы, до которых нельзя допрыгнуть
+ways_to_come.append(0)
+ways_to_come.append(0)
+ways_to_come.append(0)
+
+k = 3 # Сила прыжка
+position = 3 # Элемент, до которого нужно определить лучший способ добраться
+
+for i in range(position, len(ways_to_come)):
+	# Определяем предшествующие текущему элементы
+	previous_elements = []
+	for j in range(k):
+		previous_elements.append()
+
+	ways_to_come[i] = max()
+
--- Term 4/Lessons/dynamic programming/2_ATM/ATM.py
+++ Term 4/Lessons/dynamic programming/2_ATM/ATM.py
@@ -1,66 +1,71 @@
-# Задача - банкомат.
-# Найти минимальное число банкнот, которыми можно выдать запрашиваемую сумму
+# # Задача - банкомат.
+# # Найти минимальное число банкнот, которыми можно выдать запрашиваемую сумму
 
-import math
+# import math
 
-banknotes = [30, 50, 100] # Доступные банкноты
-sum = 210 # Сумма для размена
+# banknotes = [30, 50, 100] # Доступные банкноты
+# sum = 210 # Сумма для размена
 
-# База (0 можно разменять 0 купюр, не получится разменять)
-# Будет хранить минимальное кол. купюр, которыми можно получить сумму =
-# значению индекса, в котором хранится число купюр
-# ВАЖНО: Если тестируешь на маленькой сумме, то измени БАЗУ
-best_exchanges = [math.inf] * (sum + 1)
+# # База (0 можно разменять 0 купюр, не получится разменять)
+# # Будет хранить минимальное кол. купюр, которыми можно получить сумму =
+# # значению индекса, в котором хранится число купюр
+# # ВАЖНО: Если тестируешь на маленькой сумме, то измени БАЗУ
+# best_exchanges = [math.inf] * (sum + 1)
 
-# Установка базы
-best_exchanges[0] = 0
-for i in banknotes:
-	best_exchanges[i] = 1
+# # Установка базы
+# best_exchanges[0] = 0
+# for i in banknotes:
+# 	best_exchanges[i] = 1
 
 
-def ATM_min_exchange(banknotes, best_exchanges, sum):
-	# Если лучший исход для запрашиваемой суммы ещё не определён
-	if best_exchanges[sum] == math.inf:
-		for sum_for_exchange in range(1, sum + 1): # Определение суммы, которую нужно разменять
-			# Сколькими купюрами возможно выдать запрашиваеммую сумму
-			pos_best_exchanges = []
+# def ATM_min_exchange(banknotes, best_exchanges, sum):
+# 	# Если лучший исход для запрашиваемой суммы ещё не определён
+# 	if best_exchanges[sum] == math.inf:
+# 		for sum_for_exchange in range(1, sum + 1): # Определение суммы, которую нужно разменять
+# 			# Сколькими купюрами возможно выдать запрашиваеммую сумму
+# 			pos_best_exchanges = []
 		
-			# Рассмотрим все способы выдать запрашиваеммую сумму, начиная выдавать купюрой разного номинала
-			for banknote in banknotes:
-				try:
-					# Попытка выдать сумму используя купюру banknote
-					pos_best_exchanges.append(best_exchanges[sum_for_exchange - banknote])
-				except:
-					continue
+# 			# Рассмотрим все способы выдать запрашиваеммую сумму, начиная выдавать купюрой разного номинала
+# 			for banknote in banknotes:
+# 				try:
+# 					# Попытка выдать сумму используя купюру banknote
+# 					pos_best_exchanges.append(best_exchanges[sum_for_exchange - banknote])
+# 				except:
+# 					continue
 
-			best_exchanges[sum_for_exchange] = min(pos_best_exchanges) + 1 # +1, т.к. для выдачи нужна будет ещё 1 купюра
+# 			best_exchanges[sum_for_exchange] = min(pos_best_exchanges) + 1 # +1, т.к. для выдачи нужна будет ещё 1 купюра
 	
-	return best_exchanges[sum]
+# 	return best_exchanges[sum]
 
-def ATM_min_exchange_path(sum):
-	ans = ATM_min_exchange(banknotes, best_exchanges, sum) # Сколько купюр нужно дать
-	dig = sum # Какую сумму выдать
-	path = [] # Какие купюры давать
+# def ATM_min_exchange_path(sum):
+# 	ans = ATM_min_exchange(banknotes, best_exchanges, sum) # Сколько купюр нужно дать
+# 	dig = sum # Какую сумму выдать
+# 	path = [] # Какие купюры давать
 
-	while best_exchanges[dig] != 1: # Пока от суммы не останется часть, которую можно выдать 1 купюрой
-		for i in banknotes:
-			if (dig - i) > 0 and best_exchanges[dig - i] == ans-1:
-				# Если из суммы можно выдать часть = купюре И 
-				# для оставшейся части суммы останется отдать на 1 купюру меньше
+# 	while best_exchanges[dig] != 1: # Пока от суммы не останется часть, которую можно выдать 1 купюрой
+# 		for i in banknotes:
+# 			if (dig - i) > 0 and best_exchanges[dig - i] == ans-1:
+# 				# Если из суммы можно выдать часть = купюре И 
+# 				# для оставшейся части суммы останется отдать на 1 купюру меньше
 				
-				path.append(i) # Эта купюра используется в выдаче
-				dig = dig-i # Остаётся отдать меньше на сумму = купюре
-				ans = ans-1 # Для выдачи остаётся на 1 купюру меньше
+# 				path.append(i) # Эта купюра используется в выдаче
+# 				dig = dig-i # Остаётся отдать меньше на сумму = купюре
+# 				ans = ans-1 # Для выдачи остаётся на 1 купюру меньше
 			
-	path.append(dig) # Добавляем последнюю купюру
-	return path
+# 	path.append(dig) # Добавляем последнюю купюру
+# 	return path
 
 
-min_banknotes = ATM_min_exchange(banknotes, best_exchanges, sum)
-print("Минимальное число купюр, необходимое для выдачи " + str(sum) + " = " + str(min_banknotes))
+# min_banknotes = ATM_min_exchange(banknotes, best_exchanges, sum)
+# print("Минимальное число купюр, необходимое для выдачи " + str(sum) + " = " + str(min_banknotes))
 
-#print("Лучшие размены для всех сумм до запрашиваемой:")
-#print(best_exchanges)
+# #print("Лучшие размены для всех сумм до запрашиваемой:")
+# #print(best_exchanges)
 
-print("Купюры, которые нужно выдать:")
-print(ATM_min_exchange_path(sum))
+# print("Купюры, которые нужно выдать:")
+# print(ATM_min_exchange_path(sum))
+
+k = 10
+for j in range(k):
+	print(j)
+
